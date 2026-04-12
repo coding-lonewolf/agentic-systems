fb6dcdf94529bc2d31f87a63f6e477c092661f67
Ahad Khan <mail.ahad99@gmail.com>
2026-04-12 08:34:01 +0530

    Added index.py for Session 6 VCS

---
+++ Module1/Session-6-VCS/index.py
@@ -0,0 +1,7 @@
+# loop through a list of numbers and print only the even numbers
+sample_list = [2,3,45,23,4,86,44,67]
+
+for item in sample_list:
+    if item %2 == 0:
+        print(item)
+        
